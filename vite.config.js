fa9c1cd3d4cc4a363d425a57e7f037d73e19a6f3
FelipeGarcia <felipes.garcia1234@gmail.com>
2025-05-29 22:31:39 -0300

    Configurado Deploy para Página GitHub

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    allowedHosts: [
-      '5173-if9v2vkldzwzt8i56ktwp-d49ec1aa.manusvm.computer',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
+  base: '/Portifolio-P2/'
 })
