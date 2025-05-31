b09c38453db6f6c79d187e9cbfba5284ce7f639b
FelipeGarcia <felipes.garcia1234@gmail.com>
2025-05-31 16:34:01 -0300

    correcao build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Portifolio-P2/'
+  base: '/Portifolio_FelipeGarcia/'
 })
